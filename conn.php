9ebcb213627336961415bc0cc2f3ec1e7e35d632
GautamGhand <gautamghand820@gmail.com>
2025-08-03 11:37:58 +0530

    database updated

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn = mysqli_connect('172.16.0.6', 'admin', 'adminsecurepassword1113', 'employee_management');
+$conn = mysqli_connect('172.16.0.5', 'root', '', 'employee_management');
 
 ?>
